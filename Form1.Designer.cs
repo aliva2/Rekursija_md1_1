d4dc3310007b4280ad3ee37e88ce347b3b43a322
Anita <anita.liva39@gmail.com>
2025-02-24 11:38:07 +0200

    Pielikts aplis un kvadrts

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 59);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(316, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Aplis";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(420, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Kvadrāts";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ievadi rekursijas līmeni:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "● Aplis - ievadi skaitlis 1 - 3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(58, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "● Kvadrāts - ievadi skaitli 1 - 8";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Location = new System.Drawing.Point(75, 162);
+            this.panel1.MaximumSize = new System.Drawing.Size(400, 400);
+            this.panel1.MinimumSize = new System.Drawing.Size(400, 400);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(587, 575);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Rekursija md1-1 Anita Līva";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,18 +3,135 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Rekursija_md1_1
 {
     public partial class Form1 : Form
     {
+        Graphics g;
+        Pen circlePen = new Pen(Color.Purple, 2); 
+        Pen squarePen = new Pen(Color.DarkGreen, 2);
+        int x, y;
+        int w = 175; // platums sākumā
+        int h = 175; // augstums sākumā
         public Form1()
         {
             InitializeComponent();
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+
+            x = (panel1.Width - w) / 2; ; // X poz sākumā
+            y = (panel1.Height - h) / 2; ; // Y poz sākumā
+
+            g = panel1.CreateGraphics(); // veido vietu kur zīmēt, abus vienā
         }
+
+        // zīmēšanas rekursija aplim
+        public void DrawCircleRecursion(int fx, int fy, int fw, int fh, int depth)
+        {
+            // zīmē apli
+            g.DrawEllipse(circlePen, new Rectangle(fx, fy, fw, fh));
+
+            // ja 2 vai 3 līmenis tad katrus 90 grādus zīmē jaunu apli kas 1/2 mazāka izmēra
+            if (depth > 1)
+            {
+                int smallerRadiusW = fw / 2;
+                int smallerRadiusH = fh / 2;
+
+                //zīmē 4 jaunus apļus katrus 90 grādus
+                DrawCircleRecursion(fx + fw / 2 - smallerRadiusW / 2, fy - smallerRadiusH / 2, smallerRadiusW, smallerRadiusH, depth - 1);  // 0 
+                DrawCircleRecursion(fx + fw - smallerRadiusW / 2, fy + fh / 2 - smallerRadiusH / 2, smallerRadiusW, smallerRadiusH, depth - 1);  // 90
+                DrawCircleRecursion(fx + fw / 2 - smallerRadiusW / 2, fy + fh - smallerRadiusH / 2, smallerRadiusW, smallerRadiusH, depth - 1);  // 180 
+                DrawCircleRecursion(fx - smallerRadiusW / 2, fy + fh / 2 - smallerRadiusH / 2, smallerRadiusW, smallerRadiusH, depth - 1);  // 270 
+                 
+            }
+        }
+
+        // aplis
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int depth = int.Parse(textBox1.Text);
+            if (depth >= 1 && depth <= 5) // rekursijas līmeņi 1-5
+            {
+                g.Clear(Form1.DefaultBackColor); // notīrīt pirms zīmēt jaunu
+                DrawCircleRecursion(x, y, w, h, depth); // zīmēšanas rekursijas funkc aplim
+            }
+        }
+
+
+
+        // kvadrāts
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int depth = int.Parse(textBox1.Text);
+            if (depth >= 1 && depth <= 8) // rekursijas līmeņi 1-8
+            {
+                g.Clear(Form1.DefaultBackColor); // notīrīt pirms zīmēt jaunu
+                DrawSquareRecursion(x, y, w, h, depth); // zīmēšanas rekursijas funkc kvadrātam
+            }
+        }
+
+        // kvadrāta rekursija
+        public void DrawSquareRecursion(int fx, int fy, int fw, int fh, int depth)
+        {
+            // kvadrāta centrs
+            float cx = fx + fw / 2;
+            float cy = fy + fh / 2;
+
+            // ceļš kvadrāta zīmējumam
+            GraphicsPath path = new GraphicsPath();
+
+            // pievieno to ceļam, konvertējot float uz int
+            path.AddRectangle(new Rectangle(
+                Convert.ToInt32(fx),
+                Convert.ToInt32(fy),
+                Convert.ToInt32(fw),
+                Convert.ToInt32(fh)
+            ));
+
+            // pagriež pa 45 grādiem * (depth - 1)
+            float angle = 45f + 45f * (depth - 1); // katru reizi pa 45 grādiem
+            Matrix matrix = new Matrix();
+            matrix.RotateAt(angle, new PointF(cx, cy));  // griež apkārt centra punktam
+            path.Transform(matrix);  // pieliek rotāciju ceļam
+
+            // uzzīmē kvadrātu
+            g.DrawPath(squarePen, path);
+
+            // ja 2+ līmenis zīmē kvadrātu
+            if (depth > 1)
+            {
+                // katrs nākamais kvadrāts būs mazāks 
+                float scaleFactor = (float)Math.Sqrt(2); // par cik samazināt
+                float smallerSizeW = fw / scaleFactor; // platuma samazināšana
+                float smallerSizeH = fh / scaleFactor; // augstuma samazināšana
+
+                // jaunā kvadrāta novietojums
+                float offsetX = (fw - smallerSizeW) / 2; // horizontālā nobīde
+                float offsetY = (fh - smallerSizeH) / 2; // vertikālā nobīde
+
+                // mazāko zīmē pagrieztu 45 grādus, visus pārvērš par int, jo float neder
+                DrawSquareRecursion(
+                    Convert.ToInt32(fx + offsetX), 
+                    Convert.ToInt32(fy + offsetY), 
+                    Convert.ToInt32(smallerSizeW), 
+                    Convert.ToInt32(smallerSizeH), 
+                    depth - 1
+                );
+            }
+        }
+
+
+
     }
+
+
+
+
 }
